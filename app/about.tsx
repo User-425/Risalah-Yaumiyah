30f998e7af6a340a5adfa35f01274fb63d05b62c
User-425 <user425.itsme@gmail.com>
2024-07-25 20:41:23 +0700

    change: reset code for maintaining

--- app/about.tsx
+++ app/about.tsx
@@ -1,109 +1,20 @@
-import React, { useEffect, useState } from 'react';
-import { StyleSheet, View, Text, Image,Dimensions, ScrollView, TouchableOpacity } from 'react-native';
-import { Colors, Typography, Card } from 'react-native-ui-lib';
-import Carousel from 'react-native-snap-carousel';
-import Icon from 'react-native-vector-icons/MaterialIcons';
-import moment from 'moment';
+import React from 'react';
+import { StyleSheet, View, ScrollView,  } from 'react-native';
+import { Button, Checkbox, Colors, Text } from 'react-native-ui-lib';
+
 import Header from '../components/Header';
-import { useRouter } from 'expo-router';
 
-import 'moment/locale/id';
-import 'moment-hijri';
-
-import ContentList from '../components/ContentList';
-import { getContents, Content } from '../modules/contentModule';
-
-const { width: viewportWidth } = Dimensions.get('window');
-
-const carouselItems = [
-  { title: 'Beautiful Beach', image: require('@/assets/images/image_1.jpg') },
-  { title: 'Do\'a Sehari-hari', image: require('@/assets/images/doa.jpg') },
-  { title: 'Alqur\'an', image: require('@/assets/images/alquran.png') },
-];
-
-const recentOpened = [
-  { name: 'Istighosah' },
-  { name: 'Yasin Fadhilah' },
-  { name: 'Do\'a Setelah Adzan' },
-];
-
-const featuredIcons = [
-  { title: 'Al-Qur\'an', icon: 'book' },
-  { title: 'Jadwal Shalat', icon: 'alarm' },
-  { title: 'Kalender', icon: 'calendar-today' },
-];
-
-const renderCarouselItem = ({ item }) => (
-  <View style={styles.carouselItem}>
-    <Image source={item.image} style={styles.carouselImage} />
-    <Text style={styles.carouselTitle}>{item.title}</Text>
-  </View>
-);
-
-const Home = () => {
-  const [currentTime, setCurrentTime] = useState('');
-  const [currentDate, setCurrentDate] = useState('');
-  const [currentHijriDate, setCurrentHijriDate] = useState('');
-
-  const [contents, setContents] = useState<Content[]>([]);
-  const router = useRouter();
-
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-      const now = moment();
-      setCurrentTime(now.format('HH:mm:ss'));
-      setCurrentDate(now.format('dddd, DD MMMM YYYY'));
-      setCurrentHijriDate(now.format('iYYYY/iM/iD'));
-    }, 1000);
-
-    return () => clearInterval(intervalId);
-  }, []);
-
-  useEffect(() => {
-    setContents(getContents());
-  }, []);
-
-  const handleSelect = (id: number) => {
-    router.push(`reader/${id}`);
-  };
-
+const About = () => {
+ 
   return (
     <View style={styles.container}>
       <View>
-        <Header title="Beranda" />
+        <Header title="About" />
       </View>
       <ScrollView>
-        <View style={styles.timeContainer}>
-          <Text style={styles.timeText}>{currentTime}</Text>
-          <Text style={styles.dateText}>{currentDate}</Text>
-          <Text style={styles.hijriDateText}>(tanggal hijriyah)</Text>
-        </View>
-        <Carousel
-          data={carouselItems}
-          renderItem={renderCarouselItem}
-          sliderWidth={viewportWidth}
-          itemWidth={viewportWidth * 0.8}
-          autoplay={true}
-          loop={true}
-        />
-        <View style={styles.featuredGrid}>
-          {featuredIcons.map((item, index) => (
-            <TouchableOpacity key={index} style={styles.iconButton}>
-              <Icon name={item.icon} size={25} color={Colors.blue30} />
-              <Text style={styles.iconTitle}>{item.title}</Text>
-            </TouchableOpacity>
-          ))}
-        </View>
-        <Text style={styles.sectionTitle}>Baru Dibuka</Text>
-        <ScrollView horizontal showsHorizontalScrollIndicator={false}>
-          {recentOpened.map((destination, index) => (
-            <Card key={index} style={styles.card}>
-              <Text style={styles.cardTitle}>{destination.name}</Text>
-            </Card>
-          ))}
-        </ScrollView>
-        <Text style={styles.sectionTitle}>More Items</Text>
-        <ContentList contents={contents} onSelect={handleSelect} />
+      <Text text30 red30>Text goes here</Text>
+      <Checkbox value={false} onValueChange={() => console.log('value changed')}/>
+      <Button label={'Press'} size={Button.sizes.medium} backgroundColor={Colors.red30}/>
       </ScrollView>
     </View>
   );
@@ -114,82 +25,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: Colors.white,
   },
-  timeContainer: {
-    alignItems: 'center',
-    paddingVertical: 20,
-  },
-  timeText: {
-    ...Typography.text30,
-    fontWeight: 'bold',
-  },
-  dateText: {
-    ...Typography.text60,
-    marginVertical: 5,
-  },
-  hijriDateText: {
-    ...Typography.text70,
-    color: Colors.grey50,
-  },
-  carouselItem: {
-    borderRadius: 8,
-    overflow: 'hidden',
-  },
-  carouselImage: {
-    width: '100%',
-    height: 200,
-  },
-  carouselTitle: {
-    ...Typography.text60,
-    color: Colors.white,
-    position: 'absolute',
-    bottom: 10,
-    left: 10,
-  },
-  sectionTitle: {
-    ...Typography.text50,
-    paddingHorizontal: 20,
-    paddingTop: 20,
-  },
-  card: {
-    margin: 10,
-    borderRadius: 8,
-    overflow: 'hidden',
-  },
-  cardImage: {
-    width: 150,
-    height: 150,
-  },
-  cardTitle: {
-    ...Typography.text70,
-    padding: 10,
-  },
-  featuredGrid: {
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    justifyContent: 'space-around',
-    paddingVertical: 10,
-  },
-  iconButton: {
-    alignItems: 'center',
-    margin: 10,
-    width: viewportWidth / 3 - 20,
-  },
-  iconTitle: {
-    ...Typography.text80,
-    marginTop: 10,
-  },
-  textItemList: {
-    paddingHorizontal: 20,
-  },
-  textItem: {
-    backgroundColor: Colors.grey60,
-    padding: 15,
-    borderRadius: 8,
-    marginBottom: 10,
-  },
-  textItemTitle: {
-    ...Typography.text70,
-  },
 });
 
-export default Home;
+export default About;
